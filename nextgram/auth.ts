f0e57421c183b3678c37a224e9826ec42bb8d3d9
Lucas Rosa <lucasmetron@gmail.com>
2025-05-22 14:17:57 -0300

    feat: adicionar página de login com autenticação do Google

--- nextgram/auth.ts
+++ nextgram/auth.ts
@@ -2,6 +2,14 @@ import type { NextAuthConfig } from "next-auth";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-const config = { providers: [Google] } satisfies NextAuthConfig;
+const config = {
+  providers: [Google],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error",
+    verifyRequest: "/auth/verify-request",
+  },
+} satisfies NextAuthConfig;
 
 export const { auth, handlers, signIn, signOut } = NextAuth(config);
---
+++ nextgram/src/app/auth/signin/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { FaGoogle } from "react-icons/fa";
+import { signIn } from "next-auth/react";
+
+export default function SignIn() {
+  return (
+    <div className="bg-white w-full h-full flex justify-start items-center p-5 flex-col gap-5">
+      <h1 className="text-black text-2xl font-bold">
+        Acesse ou crie uma conta nas opções disponíveis
+      </h1>
+
+      <button
+        onClick={() => {
+          signIn("", { callbackUrl: "/" });
+        }}
+        className="border-2 border-black rounded-md py-2 px-5 text-center text-black flex gap-2 items-center cursor-pointer"
+      >
+        <FaGoogle />
+        <p>
+          Entre com o <b>Google</b>
+        </p>
+      </button>
+    </div>
+  );
+}

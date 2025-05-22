f5137120ca3a14dfd0e4802e787677eff2e566e3
Lucas Rosa <lucasmetron@gmail.com>
2025-05-22 16:59:05 -0300

    feat: adicionar largura e altura completas ao contêiner da página inicial

--- nextgram/src/app/page.tsx
+++ nextgram/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-white overflow-auto flex flex-col gap-2">
+    <div className="bg-white overflow-auto flex flex-col gap-2 w-full h-full">
       <p>home</p>
     </div>
   );
